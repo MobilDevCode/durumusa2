11e15186145922aba78aab00418b81a2c9e1ae56
MobilDevcODE <k.erenmaden11@gmail.com>
2024-09-16 22:33:09 +0300

    ASD

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { MainNavigation } from "./app/navigations/MainNavigation";
+import { BluetoothProvider } from "./app/context/BluetoothContext";
 
 export default function App() {
+  useEffect(() => {}, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BluetoothProvider>
+      <MainNavigation />
+    </BluetoothProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,39 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          extensions: [
+            ".js",
+            ".jsx",
+            ".ts",
+            ".tsx",
+            ".ios.js",
+            ".ios.jsx",
+            ".ios.ts",
+            ".ios.tsx",
+            ".android.js",
+            ".android.jsx",
+            ".android.ts",
+            ".android.tsx",
+            ".json",
+          ],
+          alias: {
+            "@": "./",
+            "@utils": "./app/utils",
+            "@icons": "./app/icons",
+            "@config": "./app/config",
+            "@fonts": "./assets/fonts",
+            "@screens": "./app/screens",
+            "@context": "./app/context",
+            "@components": "./app/components",
+            "@navigations": "./app/navigations",
+          },
+        },
+      ],
+    ],
   };
 };
